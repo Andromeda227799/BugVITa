ad94bf99df3c374ec4229840601a7cff1700b527
Shamith Rao <shamith2709@gmail.com>
2021-05-12 12:27:01 +0530

    bhai addbugdone

--- client/src/components/BugsAddComp.js
+++ client/src/components/BugsAddComp.js
@@ -1,6 +1,7 @@
 import { useDispatch } from "react-redux";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
+import { BugAdded } from "../redux/ducks/bugsAddDuck";
 
 import {
   showBugsList,
@@ -16,7 +17,7 @@ const BugsAddComp = () => {
 
   const [textInput_bug, setTextInput_bug] = useState("");
   const [textInput_author, setTextInput_author] = useState("");
-  var temp;
+
   const AddBugHandler = () => {
     const bugAdded = {
       name: textInput_bug,
@@ -25,33 +26,10 @@ const BugsAddComp = () => {
       description: textInput_bug,
     };
 
-    console.log(`${textInput_author} ${textInput_bug}`);
-    axios
-      .post(`/api/products/bugs`, bugAdded)
-      .then((response) => {
-        console.log(response);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    axios
-      .get(`/api/products/${selectedProduct}`) //fetch bugs belonging to the specified product
-      .then((response) => {
-        const bugs = response.data;
-        //We successfully got a response,so we'll dispatch:
-        console.log(bugs);
-      })
-      .catch((error) => {
-        const errorMsg = error.message;
-        //We were not able to get data because of some error. So we dispatch:
-      });
-
+    dispatch(BugAdded(bugAdded));
     //HERE textInput_bug has the bug description and textInput_author has the respective author name
     // You need to add these to the buglist database which idk how to do lols,probablt teach me later!
-
     //ADD YOUR CODE HERE{textInput_bug,textInput_author}
-
     setTextInput_bug("");
     setTextInput_author("");
     dispatch(BugAddedScreen());
